682b6f201f17babbe48483f6f38e8c1cbfa12cb0
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-05-24 15:24:00 +0300

    useCallback example

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component,useState,useEffect} from 'react';
+import {Component,useState,useEffect,useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
@@ -28,6 +28,14 @@ const Slider= (props) => {
 
     const [slide,setSlide]=useState(()=>calcValue()); // obj, string,numbers.. for slide
     const [autoplay,setAutoplay]=useState(false);
+
+    const getSomeImages=useCallback(()=>{
+        console.log('fetching');
+        return [
+             'https://hcsalavat.ru/upload/iblock/b5a/grhqxdtfoo1nh4nm0x3pwo18qiwdvmzj.png',
+             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzYGCzee47v4L_ajZISH1Ah2eqoVKtxU6YiAzF4pmvtm4zHuyhG0A70r3wWu2sgY4u790&usqp=CAU'    
+            ]}, []) 
+            // мемоизир ф-ия
 
     function logging(){
         console.log('log!')
@@ -61,6 +69,15 @@ const Slider= (props) => {
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                    {/* {
+                        getSomeImages().map((url,i)=>{
+                            return(
+                                <img key={i} className='d-block w-100' src={url} alt='slide'></img>
+                            )
+                        })
+                    } */}
+                    <Slide getSomeImages={getSomeImages}/>
+
                 <div className="text-center mt-5">Active slide {slide} <br/> 
                 {autoplay ? 'auto' : null}  
                 {/* строка режима включения */}
@@ -80,7 +97,22 @@ const Slider= (props) => {
         </Container>
     )
 }
+const Slide=({getSomeImages})=>{
+    const [images,setImages]=useState([]);
 
+    useEffect(()=>{
+        setImages(getSomeImages())
+    },[getSomeImages]) // когда эта ф-ия изменится, то только тогда useEffect запуск-ся повторно
+
+    return(
+        <>
+            {images.map((url,i)=>{
+                <img key={i} className='d-block w-100' src={url} alt='slide'></img>          
+            })}
+                                
+        </>
+    )
+}
 
 function App() {
 
